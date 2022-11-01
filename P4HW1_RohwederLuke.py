a2946adf7482d08335c5c5a5ca58a1c559d8d745
LukeRohweder <113685095+LukeRohweder@users.noreply.github.com>
2022-11-01 13:49:28 -0400

    P4HW1_RohwederLuke
    P4HW1

---
+++ P4HW1_RohwederLuke.py
@@ -0,0 +1,21 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+help
+Type help() for interactive help, or help(object) for help about object.
+help()
+
+Welcome to Python 3.10's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.10/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> 
